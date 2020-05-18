e2b3f6a4122d230bdc8eee83ef7cca4afe315a1a
Juan Diego Campuzano <diegocampuzano62@gmail.com>
2020-05-18 15:15:56 -0500

    configs

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
-print ('chupelo')
+import pygame
+from config import *
+
+if __name__ == '__main__':
+
+    pygame.init()
+    ventana = pygame.display.set_mode([ANCHO,ALTO])
+
+    fin = False
+
+    while not fin:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin = True
