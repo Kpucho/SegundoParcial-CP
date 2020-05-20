557cd1ab09c6cf3565036e69ae2b69d8dc84acea
Cristian Rodriguez <43106928+NOZYP@users.noreply.github.com>
2020-05-20 16:41:01 -0500

    preliminares

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-ANCHO = 1100
+"""                         Constantes                  """
+ANCHO = 800
 ALTO = 700
+
+#Colores
+VERDE = [0,255,0]
+NEGRO=[0,0,0]
+
+
+#Control
+FPS = 40
--- game.py
+++ game.py
@@ -1,14 +1,56 @@
 import pygame
 from config import *
 
-if __name__ == '__main__':
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,40])
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = ALTO/2
+
+    def Right(self):
+        self.rect.x = 200
+
+    def Left(self):
+        self.rect.x = 100
+
+    def update(self):
+        pass
+
+"""                          WORLD                      """
+def World():
+    pass
+
+
+if __name__ == '__main__':
     pygame.init()
     ventana = pygame.display.set_mode([ANCHO,ALTO])
 
-    fin = False
+    Jugadores = pygame.sprite.Group()
 
+    J = Player()
+    Jugadores.add(J)
+
+    reloj = pygame.time.Clock()
+    fin = False
     while not fin:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin = True
+            if event.type ==pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    J.Right()
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    J.Left()
+
+
+
+        #CONTROL
+        Jugadores.update()
+        ventana.fill(NEGRO)
+        Jugadores.draw(ventana)
+        pygame.display.flip()
+        reloj.tick(FPS)
