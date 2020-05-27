2764e0b64eac7bda906c6e71043ec8f54f452c32
Victor <roymason11v1@gmail.com>
2020-05-26 22:15:40 -0500

    Version 0.0 carriles

--- game.py
+++ game.py
@@ -5,22 +5,59 @@ from config import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([40,40])
+        self.image = pygame.Surface([32,32])
         self.image.fill(VERDE)
         self.rect = self.image.get_rect()
         self.rect.x = 100
         self.rect.y = ALTO/2
+        self.velx = 0
+        self.vely = 0
+        self.rapidez = 7
+        self.vida = 3
+        # self.bloques = None
 
-    def Right(self):
-        self.rect.x = 200
+    def update_vel(self):
 
-    def Left(self):
-        self.rect.x = 100
+        if j.vely > 0:
+            j.vely = j.rapidez
+        elif j.vely < 0:
+            j.vely = - j.rapidez
+
+        if j.velx > 0:
+            j.velx = j.rapidez
+        elif j.velx < 0:
+            j.velx = - j.rapidez
+
 
     def update(self):
-        pass
+        self.rect.x += self.velx
 
-"""                                                 WORLD                                                       """
+        self.rect.y+=self.vely
+
+        #Franjas negras
+        if self.rect.bottom >= 600:
+            self.vely=0
+            self.rect.bottom = 600
+        if self.rect.top <= 100:
+            self.vely=0
+            self.rect.top = 100
+
+        #Penalizacion por fango o arena
+        if self.rect.top <= 200 and self.rect.bottom >= 100:
+            self.rapidez = 2
+        elif self.rect.top <= 600 and self.rect.bottom >= 500:
+            self.rapidez = 2
+
+        elif self.rect.top > 100 and self.rect.bottom < 600:
+            self.rapidez = 7
+
+        self.update_vel()
+
+
+
+
+
+"""                          WORLD                                        """
 def World():
     pass
 
@@ -29,11 +66,13 @@ def draw_text(msj, font, color, surface, cord):
     surface.blit(object, cord)
 
 
+
+
 if __name__ == '__main__':
 
     ventana = pygame.display.set_mode([ANCHO,ALTO])
 
-    """                                             MENU                                                        """
+    """                       MENU                                        """
     pygame.font.init()
     pygame.mixer.init(44100, -16, 2, 2048)
     fuente = pygame.font.Font(None, 40)
@@ -73,31 +112,49 @@ if __name__ == '__main__':
         click = False
 
 
-    """                                              JUEGO                                                       """
+    """//////////////                      JUEGO                            ///////////"""
     musica.stop()
 
     Jugadores = pygame.sprite.Group()
 
-    J = Player()
-    Jugadores.add(J)
+    j = Player()
+    Jugadores.add(j)
 
     reloj = pygame.time.Clock()
     fin_juego = False
+
+
     while not fin and (not fin_juego):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 fin = True
             if event.type ==pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    J.Right()
-                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    J.Left()
+                if event.key == pygame.K_DOWN:
+                    j.velx = 0
+                    j.vely = j.rapidez
+                if event.key == pygame.K_UP:
+                    j.velx = 0
+                    j.vely = - j.rapidez
+                if event.key == pygame.K_RIGHT:
+                    j.velx = j.rapidez
+                    j.vely = 0
+                if event.key == pygame.K_LEFT:
+                    j.velx = - j.rapidez
+                    j.vely = 0
 
 
 
         #CONTROL
         Jugadores.update()
+
+        #Dibujado
         ventana.fill(NEGRO)
+        pygame.draw.line(ventana, AMARILLO, [0, 100], [ANCHO, 100])
+        pygame.draw.line(ventana, AMARILLO, [0, 200], [ANCHO, 200])
+        pygame.draw.line(ventana, AZUL, [0, 300], [ANCHO, 300])
+        pygame.draw.line(ventana, AZUL, [0, 400], [ANCHO, 400])
+        pygame.draw.line(ventana, AMARILLO, [0, 500], [ANCHO, 500])
+        pygame.draw.line(ventana, AMARILLO, [0, 600], [ANCHO, 600])
         Jugadores.draw(ventana)
         pygame.display.flip()
         reloj.tick(FPS)
